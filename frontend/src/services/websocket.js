acf3428611134ac3726d9e1f69a2bf01386c5154
佳伟 <s2265681@163.com>
2026-05-02 03:56:14 +0800

    feat: 添加前端WebSocket客户端服务

---
+++ frontend/src/services/websocket.js
@@ -0,0 +1,89 @@
+import { io } from 'socket.io-client';
+import { apiConfig } from '../config/api';
+
+class StockWebSocket {
+  constructor() {
+    this.socket = null;
+    this._callbacks = {
+      l2Update: [],
+      disconnect: [],
+      connect: [],
+    };
+  }
+
+  connect() {
+    if (this.socket?.connected) return;
+
+    this.socket = io(apiConfig.baseURL, {
+      transports: ['websocket', 'polling'],
+      reconnection: true,
+      reconnectionDelay: 1000,
+      reconnectionAttempts: 10,
+    });
+
+    this.socket.on('connect', () => {
+      console.log('[WS] 已连接');
+      this._callbacks.connect.forEach(cb => cb());
+    });
+
+    this.socket.on('disconnect', (reason) => {
+      console.log('[WS] 已断开:', reason);
+      this._callbacks.disconnect.forEach(cb => cb(reason));
+    });
+
+    this.socket.on('l2_update', (data) => {
+      this._callbacks.l2Update.forEach(cb => cb(data));
+    });
+
+    this.socket.on('error', (data) => {
+      console.error('[WS] 服务端错误:', data.message);
+    });
+  }
+
+  disconnect() {
+    if (this.socket) {
+      this.socket.disconnect();
+      this.socket = null;
+    }
+  }
+
+  subscribe(code) {
+    if (this.socket?.connected) {
+      this.socket.emit('subscribe', { code });
+    }
+  }
+
+  unsubscribe() {
+    if (this.socket?.connected) {
+      this.socket.emit('unsubscribe');
+    }
+  }
+
+  onL2Update(callback) {
+    this._callbacks.l2Update.push(callback);
+    return () => {
+      this._callbacks.l2Update = this._callbacks.l2Update.filter(cb => cb !== callback);
+    };
+  }
+
+  onConnect(callback) {
+    this._callbacks.connect.push(callback);
+    return () => {
+      this._callbacks.connect = this._callbacks.connect.filter(cb => cb !== callback);
+    };
+  }
+
+  onDisconnect(callback) {
+    this._callbacks.disconnect.push(callback);
+    return () => {
+      this._callbacks.disconnect = this._callbacks.disconnect.filter(cb => cb !== callback);
+    };
+  }
+
+  isConnected() {
+    return this.socket?.connected || false;
+  }
+}
+
+// 单例
+export const stockWS = new StockWebSocket();

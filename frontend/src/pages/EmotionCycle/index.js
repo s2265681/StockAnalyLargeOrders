206f9174a47ccf818d12b8d36386fec0c1d2c5b6
佳伟 <s2265681@163.com>
2026-05-20 19:43:31 +0800

    feat: wire MarketBriefBar into EmotionCycle page

--- frontend/src/pages/EmotionCycle/index.js
+++ frontend/src/pages/EmotionCycle/index.js
@@ -14,6 +14,7 @@ import {
 } from 'echarts/components';
 import { CanvasRenderer } from 'echarts/renderers';
 import { apiRequest } from '../../config/api';
+import MarketBriefBar from '../../components/MarketBriefBar';
 import './index.css';
 
 
@@ -431,6 +432,7 @@ function EmotionCycle() {
 
   return (
     <div className="emotion-cycle-container">
+      <MarketBriefBar />
       <div className="page-date-nav">
         <button
           type="button"
